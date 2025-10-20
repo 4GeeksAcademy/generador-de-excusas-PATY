4405b90f5f85ed0793c0ea1b7b8b36a4fe5b792c
Paty81 <molinamartosp@gmail.com>
2025-10-20 21:59:01 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-
 window.onload = function() {
-  const lista = document.querySelectorAll("#lista-excusas li");
-  const indice = Math.floor(Math.random() * lista.length);
-  const excusa = lista[indice].textContent;
-  document.getElementById("excusa").textContent = excusa;
+let quien = ['Mi coche', 'Mi madre', 'Un alien', 'Mi gato'];
+let accion = ['se comió', 'quemó', 'aterrizó', 'se rompio'];
+let que = ['mi proyecto', 'el ordenador', 'una pata' , 'mi casa'];
+let cuando = ['antes de salir', 'esta mañana', 'hace cinco minutos', 'mientras me duchaba']
+;
+let randomquien = quien[Math.floor(Math.random() * quien.length)];
+let randomaccion = accion[Math.floor(Math.random() * accion.length)];
+let randomque = que[Math.floor(Math.random() * que.length)];
+let randomcuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+let excusa = randomquien + " " + randomaccion + " " + randomque + " " + randomcuando;
+document.getElementById("excuse").innerHTML = excusa;
+   console.log(excusa);
+  
 };
 
 
